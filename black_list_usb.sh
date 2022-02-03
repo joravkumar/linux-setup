35120d23ec0faf6f6832594b4d0d6124d67d8b5d
Jorav <42232691+joravkumar@users.noreply.github.com>
2022-02-03 13:10:47 +0530

    Update black_list_usb.sh

--- black_list_usb.sh
+++ black_list_usb.sh
@@ -2,3 +2,5 @@
 
 echo $'\nblacklist usb_storage' >> /etc/modprobe.d/blacklist.conf
 echo $'\nblacklist uas' >> /etc/modprobe.d/blacklist.conf
+
+mv /lib/modules/$(uname -r)/kernel/drivers/usb/storage/usb-storage.ko.xz /lib/modules/$(uname -r)/kernel/drivers/usb/storage/usb-storage.ko.xz.blacklist
